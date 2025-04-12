85e99a85a4f22ae40050c8cfb9adac2c8dbd92ec
Prabhat Tiwari <41245023-prabhattiwari98@users.noreply.replit.com>
2025-04-12 13:31:54 +0000

    Restored to 'cd1e86ab58a1fb38edc011bb86fd99b3e856d5e1'
    Replit-Restored-To: cd1e86ab58a1fb38edc011bb86fd99b3e856d5e1

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-from flask import Flask
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'your-secret-key'  # Change this to a secure value
-app.config['TEMPLATES_AUTO_RELOAD'] = True
+from app import app
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
